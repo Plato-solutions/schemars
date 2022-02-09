d85633cbe3993f568dce6f8cd52cb59b7c32cd17
Ryan Miller <7181449-platosolutions@users.noreply.gitlab.com>
2022-02-09 18:09:50 -0500

    PrimitiveDateTime did not work, this updates it and is now passing automated tests but need manual testing

--- schemars/src/json_schema_impls/time03.rs
+++ schemars/src/json_schema_impls/time03.rs
@@ -29,6 +29,27 @@ macro_rules! formatted_string_impl {
 
 formatted_string_impl!(Date, "date");
 formatted_string_impl!(Time, "time");
-formatted_string_impl!(PrimitiveDateTime, "partial-date-time");
 formatted_string_impl!(OffsetDateTime, "date-time");
 forward_impl!(Duration => std::time::Duration);
+
+//
+impl JsonSchema for PrimitiveDateTime {
+    fn schema_name() -> String {
+        "PrimitiveDateTime".to_owned()
+    }
+
+    fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+        let mut schema = SchemaObject {
+            instance_type: Some(InstanceType::Object.into()),
+            ..Default::default()
+        };
+        let obj = schema.object();
+        obj.required.insert("date".to_owned());
+        obj.required.insert("time".to_owned());
+        obj.properties
+            .insert("date".to_owned(), <Date>::json_schema(gen));
+        obj.properties
+            .insert("time".to_owned(), <Time>::json_schema(gen));
+        schema.into()
+    }
+}
--- schemars/tests/time03.rs
+++ schemars/tests/time03.rs
@@ -1,15 +1,18 @@
 mod util;
 use schemars::JsonSchema;
-use time::OffsetDatTime;
+use time03::{Date,Time,PrimitiveDateTime,OffsetDateTime,Duration};
 use util::*;
-//
-// #[derive(Debug, JsonSchema)]
-// struct MyStruct {
-//     duration: Duration,
-//     time: SystemTime,
-// }
-//
-// #[test]
-// fn duration_and_systemtime() -> TestResult {
-//     test_default_generated_schema::<MyStruct>("duration_and_systemtime")
-// }
+
+#[derive(Debug, JsonSchema)]
+struct Time03Types {
+    date: Date,
+    time: Time,
+    primitive_date: PrimitiveDateTime,
+    offset_date_time: OffsetDateTime,
+    duration: Duration,
+}
+
+#[test]
+fn time03_types() -> TestResult {
+    test_default_generated_schema::<Time03Types>("time03-types")
+}
