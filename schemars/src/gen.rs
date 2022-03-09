a3d3321b0caee949e4e7d922144c399fd87925b3
Ryan Miller <7181449-platosolutions@users.noreply.gitlab.com>
2022-03-09 03:18:50 -0500

    ?

--- schemars/src/gen.rs
+++ schemars/src/gen.rs
@@ -94,7 +94,7 @@ impl SchemaSettings {
             visitors: vec![
                 Box::new(RemoveRefSiblings),
                 Box::new(ReplaceBoolSchemas {
-                    skip_additional_properties: true,
+                    skip_additional_properties: false,
                 }),
                 Box::new(SetSingleExample {
                     retain_examples: false,
